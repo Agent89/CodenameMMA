f47b6a48d66310aa03b5e190d66e33d218728e7a
Jeremy Jones <psxdude_007@hotmail.com>
2012-01-15 21:09:17 -0800

    Started new template system

--- js/script.js
+++ js/script.js
@@ -2,3 +2,18 @@
 
 */
 
+function getUserName(UserID,fieldID) {
+    var params = new Object();
+    params.UserID = UserID;
+    $.ajax(
+        {
+            type: "POST",
+            dataType: "json",
+            contentType: "application/json",
+            url: "MMACode.asmx/GetUserName",
+            data: $.toJSON(params),
+            success: function (r) {
+                $("#"+fieldID).text(r.d);
+            }
+        });
+}
